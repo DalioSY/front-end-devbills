0a0a0ca3fc49194b7e3d0f4132a7ec128990de0d
DalioSY <dalioyamada@gmail.com>
2024-04-20 18:02:39 -0300

    front-end-codebills

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: "#003bed",
+    primarydark: "#002d68",
+    success: "#1c8c2f",
+    info: "#004bed",
+    warning: "#f7b731",
+    error: "#671717",
+    white: "#d1d1d1",
+    light: "#abadac",
+    neutral: "#7f8483",
+    dark: "#001822",
+    black: "#040613",
+  },
+};
